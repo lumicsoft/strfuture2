ce239336ff7c4eb983503461230ff065954b2691
lumicsoft <lumicsoft@gmail.com>
2026-03-08 11:33:31 +0530

    Update web3-handler.js

--- web3-handler.js
+++ web3-handler.js
@@ -1,7 +1,7 @@
 let provider, signer, contract, userAddress;
 
 // --- CONFIGURATION ---
-const CONTRACT_ADDRESS = "0xf94f2EBb6ae0250800e6391eBA6Bd5f62BC6732a"; 
+const CONTRACT_ADDRESS = "0x46Bf614435228fC166D18eC3Ca6121B53329D0Da"; 
 const USDT_TOKEN_ADDRESS = "0x3b66b1e08f55af26c8ea14a73da64b6bc8d799de"; // BSC USDT
 const TESTNET_CHAIN_ID = 97; 
 const REGISTRATION_FEE = "15";
@@ -599,6 +599,7 @@ function updateNavbar(addr) {
 }
 
 window.addEventListener('load', init);
+
 
 
 
